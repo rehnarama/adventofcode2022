baefdd1fd4af438ff4cd090b0ca2d548033588f0
Astrid Rehn <1352555+rehnarama@users.noreply.github.com>
2022-12-18 21:23:55 +0100

    Add day11+12

---
+++ src/day11/part2.rs
@@ -0,0 +1,235 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum Operation {
+    Divide(u64),
+    Add(u64),
+    Multiply(u64),
+    AddSelf,
+    MultiplySelf,
+}
+
+impl From<(&str, &str)> for Operation {
+    fn from(args: (&str, &str)) -> Self {
+        let arg = args.1.parse::<u64>();
+
+        match args.0 {
+            "*" => {
+                if arg.is_ok() {
+                    Operation::Multiply(arg.unwrap())
+                } else {
+                    Operation::MultiplySelf
+                }
+            }
+            "+" => {
+                if arg.is_ok() {
+                    Operation::Add(arg.unwrap())
+                } else {
+                    Operation::AddSelf
+                }
+            }
+            "/" => Operation::Divide(arg.unwrap()),
+            _ => panic!("Unknown operation"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Item {
+    worry_level: u64,
+}
+
+impl Item {
+    fn apply_op(&mut self, operation: &Operation) {
+        self.worry_level = match operation {
+            Operation::Divide(arg) => self.worry_level / arg,
+            Operation::Add(arg) => self.worry_level + arg,
+            Operation::Multiply(arg) => self.worry_level * arg,
+            Operation::AddSelf => self.worry_level + self.worry_level,
+            Operation::MultiplySelf => self.worry_level * self.worry_level,
+        };
+    }
+
+    fn test_op(&self, operation: &Operation) -> bool {
+        match operation {
+            Operation::Divide(arg) => self.worry_level % arg == u64::from(0u32),
+            _ => panic!("Can't test other than divisible op"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Monkey {
+    name: usize,
+    items: Vec<Item>,
+    operation: Operation,
+    test: Operation,
+    truthy_target: usize,
+    falsey_target: usize,
+    n_inspections: usize,
+}
+
+impl FromStr for Monkey {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        lazy_static! {
+            static ref MonkeyRE: Regex = Regex::new(r"(\d+)").unwrap();
+            static ref ItemsRE: Regex = Regex::new(r"(\d+)").unwrap();
+            static ref OperationRE: Regex = Regex::new(r"([+*])\s?(\d+|old)").unwrap();
+            static ref TestRE: Regex = Regex::new(r"(\d+)").unwrap();
+            static ref TargetRE: Regex = Regex::new(r"(\d+)").unwrap();
+        }
+
+        let lines = s.lines().collect::<Vec<&str>>();
+        let monkey_s = lines.get(0).ok_or("Couldn't get monkey line")?;
+        let items_s = lines.get(1).ok_or("Couldn't get item line")?;
+        let operation_s = lines.get(2).ok_or("Couldn't get operation line")?;
+        let test_s = lines.get(3).ok_or("Couldn't get test line")?;
+        let truthy_target_s = lines.get(4).ok_or("Couldn't get truthy target line")?;
+        let falsey_target_s = lines.get(5).ok_or("Couldn't get falsey target line")?;
+
+        let name = MonkeyRE
+            .captures(monkey_s)
+            .map(|c| c.get(0).unwrap())
+            .unwrap()
+            .as_str()
+            .parse::<usize>()
+            .unwrap();
+
+        let items: Vec<Item> = ItemsRE
+            .captures_iter(items_s)
+            .map(|c| c.get(0).unwrap().as_str())
+            .map(|s| s.parse::<u64>().unwrap())
+            .map(|i| Item { worry_level: i })
+            .collect();
+
+        let operation: Operation = OperationRE
+            .captures(operation_s)
+            .map(|c| (c.get(1).unwrap().as_str(), c.get(2).unwrap().as_str()))
+            .map(|(op, arg)| Operation::from((op, arg)))
+            .unwrap();
+
+        let test: Operation = TargetRE
+            .captures(test_s)
+            .map(|c| c.get(0).unwrap())
+            .map(|s| s.as_str().parse::<u64>().unwrap())
+            .map(|t| Operation::Divide(t))
+            .unwrap();
+
+        let truthy_target = TargetRE
+            .captures(truthy_target_s)
+            .map(|c| c.get(0).unwrap())
+            .map(|m| m.as_str().parse::<usize>().unwrap())
+            .unwrap();
+
+        let falsey_target = TargetRE
+            .captures(falsey_target_s)
+            .map(|c| c.get(0).unwrap())
+            .map(|m| m.as_str().parse::<usize>().unwrap())
+            .unwrap();
+
+        let monkey = Monkey {
+            name,
+            items,
+            operation,
+            test,
+            truthy_target,
+            falsey_target,
+            n_inspections: 0,
+        };
+
+        Ok(monkey)
+    }
+}
+
+impl Monkey {
+    fn do_turn(&mut self, common_denominator: u64) -> Option<(usize, Item)> {
+        if (self.items.len() == 0) {
+            return None;
+        }
+
+        let mut item = self.items.remove(0);
+        self.n_inspections = self.n_inspections + 1;
+        item.apply_op(&self.operation);
+        item.worry_level = item.worry_level % common_denominator;
+
+        if item.test_op(&self.test) {
+            Some((self.truthy_target, item))
+        } else {
+            Some((self.falsey_target, item))
+        }
+    }
+
+    fn throw(&mut self, item: Item) {
+        self.items.push(item);
+    }
+}
+
+#[derive(Debug)]
+struct Simulation {
+    round: i32,
+    monkeys: Vec<Monkey>,
+    common_denominator: u64,
+}
+
+impl Simulation {
+    fn new(monkeys: Vec<Monkey>) -> Simulation {
+        let common_denominator = monkeys
+            .iter()
+            .map(|m| match m.test {
+                Operation::Divide(arg) => arg,
+                _ => panic!("Test isn't division"),
+            })
+            .fold(1, |acc, element| acc * element);
+
+        Simulation {
+            round: 0,
+            monkeys: monkeys,
+            common_denominator,
+        }
+    }
+
+    fn do_round(&mut self) {
+        for i in 0..self.monkeys.len() {
+            while let Some((target, item)) = self.do_turn_on(i) {
+                self.monkeys.get_mut(target as usize).unwrap().throw(item);
+            }
+        }
+
+        self.round = self.round + 1;
+    }
+
+    fn do_turn_on(&mut self, monkey: usize) -> Option<(usize, Item)> {
+        self.monkeys.get_mut(monkey).unwrap().do_turn(self.common_denominator)
+    }
+}
+
+fn main() {
+    let input = include_str!("input.txt").replace("\r", "");
+
+    let mut monkeys: Vec<Monkey> = input
+        .split("\n\n")
+        .map(|line| line.parse::<Monkey>().unwrap())
+        .collect();
+
+    let mut simulation = Simulation::new(monkeys);
+
+    for i in 0..10000 {
+        simulation.do_round();
+    }
+
+    let mut n_inspections: Vec<usize> = simulation
+        .monkeys
+        .iter()
+        .map(|monkey| monkey.n_inspections)
+        .collect();
+
+    n_inspections.sort_by(|a, b| b.cmp(a));
+
+    let monkey_business = n_inspections.get(0).unwrap() * n_inspections.get(1).unwrap();
+
+    dbg!(monkey_business);
+}
---
+++ src/day12/part1.rs
@@ -0,0 +1,136 @@
+use std::{collections::HashMap, fmt::Display};
+
+struct Map {
+    heights: HashMap<(usize, usize), char>,
+    size: (usize, usize),
+    start: (usize, usize),
+    end: (usize, usize),
+    scores: HashMap<(usize, usize), u32>,
+}
+
+impl Map {
+    fn new(heights: HashMap<(usize, usize), char>) -> Map {
+        let width = heights.keys().fold(0, |acc, element| acc.max(element.0));
+        let height = heights.keys().fold(0, |acc, element| acc.max(element.1));
+        let start = *heights
+            .iter()
+            .find(|(_, &height)| height == 'S')
+            .map(|(coord, _)| coord)
+            .unwrap();
+        let end = *heights
+            .iter()
+            .find(|(_, &height)| height == 'E')
+            .map(|(coord, _)| coord)
+            .unwrap();
+
+        let mut scores = HashMap::new();
+        scores.insert(end, 0);
+        Map {
+            heights: heights
+                .iter()
+                .map(|(&coord, &height)| {
+                    let height = match height {
+                        'S' => 'a',
+                        'E' => 'z',
+                        _ => height,
+                    };
+                    (coord, height)
+                })
+                .collect(),
+            size: (width, height),
+            start,
+            end,
+            scores,
+        }
+    }
+
+    fn calculate_scores(&mut self, to: (usize, usize)) {
+        let to_score = *self.scores.get(&to).unwrap();
+
+        let surrounding = self.get_surrounding(to);
+
+        for from in surrounding {
+            let from_score = to_score + 1;
+
+            let current_from_score = self.scores.get(&from);
+
+            if self.is_eligible_move(&from, &to) {
+                if current_from_score.is_some() && *current_from_score.unwrap() <= from_score {
+                    continue;
+                }
+
+                self.scores.insert(from, from_score);
+                self.calculate_scores(from);
+            }
+        }
+    }
+
+    fn is_eligible_move(&self, from: &(usize, usize), to: &(usize, usize)) -> bool {
+        let from = *self.heights.get(from).unwrap();
+        let to = *self.heights.get(to).unwrap();
+
+        (to as i32) - (from as i32) <= 1
+    }
+
+    fn get_surrounding(&self, pos: (usize, usize)) -> Vec<(usize, usize)> {
+        let mut surrounding = Vec::new();
+        for x in vec![-1, 0, 1] {
+            for y in vec![-1, 0, 1] {
+                if !(x == 0 || y == 0) || (x == 0 && y == 0) {
+                    continue;
+                }
+
+                let target_x = pos.0 as i32 + x;
+                let target_y = pos.1 as i32 + y;
+
+                let in_range_x = target_x >= 0 && target_x <= self.size.0 as i32;
+                let in_range_y = target_y >= 0 && target_y <= self.size.1 as i32;
+                if in_range_x && in_range_y {
+                    surrounding.push((target_x.try_into().unwrap(), target_y.try_into().unwrap()));
+                }
+            }
+        }
+
+        surrounding
+    }
+}
+
+impl Display for Map {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for y in 0..=self.size.1 {
+            for x in 0..=self.size.0 {
+                write!(
+                    f,
+                    ".{:0>4}",
+                    self.scores
+                        .get(&(x, y))
+                        .map(|s| s.to_string())
+                        .unwrap_or("-".to_string())
+                )?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+
+    let heights: HashMap<(usize, usize), char> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .map(move |(x, height)| ((x, y), height))
+        })
+        .collect();
+    let mut map = Map::new(heights);
+    map.calculate_scores(map.end);
+
+    let n_steps = map.scores.get(&map.start).unwrap();
+
+    dbg!(n_steps);
+}
